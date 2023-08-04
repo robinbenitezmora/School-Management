c4c5f5c5ed1b52b5239db1cf06ecc2b1e1c63a5a
robinbenitezmora <robinbenitez1990@gmail.com>
2023-08-03 20:56:57 -0500

    Update 'School.java' class

--- Student.java
+++ Student.java
@@ -7,7 +7,7 @@ public class Student {
  private int id;
  private String name;
  private int grade;
- private int feesPaid;
+ private static int feesPaid;
  private int feesTotal;
 
  /**
@@ -48,7 +48,7 @@ public class Student {
   * @param fees the fees that the student pays.
   */
 
- public void payFees(int fees) {
+ public static void payFees(int fees) {
   feesPaid += fees;
   School.updateTotalMoneyEarned(feesPaid);
  }
